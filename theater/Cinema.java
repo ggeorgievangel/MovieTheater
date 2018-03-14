e520168977c18d7edf562fe238ef747f0195f5fb
Georgiev Angel <g.georgievangel@gmail.com>
2018-03-14 13:15:25 +0200

    work on class Cinema

--- theater/Cinema.java
+++ theater/Cinema.java
@@ -1,5 +1,37 @@
 package theater;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Cinema {
-
+	// Singleton class
+	private static Cinema instance = null;
+	private String name;
+	private String address;
+	private TreeSet<Hall> halls;
+	private Set<Movie> movies;
+	
+	private Cinema() {
+		this.name = "Botevgrad Movie Theater";
+		this.address = "ul. Filmova 7";
+		for (int i = 1; i <= 5; i++) {
+			this.halls.add(new Hall(i));
+		}
+	}
+	
+	public static Cinema getInstance() {
+		if (instance == null) {
+			instance = new Cinema();
+			return instance;
+		}
+		return instance;
+	}
+	
+	public void acceptReservation(Movie m, int places) {
+		// check in which hall is this movie projected
+			// if there are enough places --> reserve 
+			// if free places < places --> message
+		
+		
+	}
+	
 }
